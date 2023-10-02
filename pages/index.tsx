a37e2c1f554e94884f966525c9bb53a5027e4041
selva <selvanithish2014@gmail.com>
2023-10-02 12:44:20 +0530

    helloworld app

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+
+  return(
+    <div className="w-screen h-screen flex justify-center items-center">
+      <p className="text-bold">Hello world!</p>
+    </div>
+  )
+
+}
